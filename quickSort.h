dcad3f03b6df901a75711abbcfe8d80762e6e7cf
tiusty <awagud@hotmail.com>
2015-02-05 16:16:41 -0500

    Creating quickSort header file for the function
    So far it only works for ints, template for strings coming SOON!!

---
+++ quickSort.h
@@ -0,0 +1,20 @@
+/*
+ * quickSort.h
+ *
+ *  Created on: Feb 5, 2015
+ *      Author: alex
+ */
+
+#ifndef QUICKSORT_H_
+#define QUICKSORT_H_
+#include <iostream>
+using namespace std;
+#include <vector>
+
+
+void quickSort (vector <int>& list, int left, int right);
+int partition(vector <int>& list, int left, int right);
+
+
+
+#endif /* QUICKSORT_H_ */
