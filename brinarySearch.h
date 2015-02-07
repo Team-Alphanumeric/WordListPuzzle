41ae3b05bfaf659290b64b5a9cbad70d11aa4ce8
tiusty <awagud@hotmail.com>
2015-02-06 19:19:04 -0500

    Created binary search
    Only works for strings, arguments are inputing the vector, word desired, int low, int high and returns true if the word exists and returns if the word doesn't exist

---
+++ brinarySearch.h
@@ -0,0 +1,18 @@
+/*
+ * binarySearch.h
+ *
+ *  Created on: Feb 6, 2015
+ *      Author: alex
+ */
+
+#ifndef BINARYSEARCH_H_
+#define BINARYSEARCH_H_
+
+using namespace std;
+#include <string>
+#include <vector>
+
+bool binSearch(vector <string> wordlist, string wordTarget, int low, int high);
+
+
+#endif /* BINARYSEARCH_H_ */
