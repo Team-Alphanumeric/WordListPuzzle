c808207fd369c44a57256d8c6c2226b57dabe32d
tiusty <awagud@hotmail.com>
2015-02-08 12:15:15 -0500

    Syntax fix/seperate files
    Seperated file into header and source and fixed syntax errors.
    Still not compiling

--- Grid.cpp
+++ Grid.cpp
@@ -1,29 +1,26 @@
+/*
+ * Grid.cpp
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: alex
+ */
+
+#include "Grid.h"
+
 /* Implements the grid class for a word search which holds and accesses a character matrix
 */
 
-#include <grid.h>
+#include "Grid.h"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 #include <iomanip>
 #include <locale>
 #include <sstream>
 #include <string>
-#include <d_except.h>
-#include <d_matrix.h>
+#include "d_execpt.h"
+#include "matrix.h"
 
-class Grid
-{
-	public:
-		Grid();
-		Grid(const string filename);
-		void setGrid(const string filename);
-		const char getChar(const int m, const int n);
-		const int getSize();
-		
-	protected:
-		matrix<char> lttrs;
-		int size;
-};
 
 const int Grid::getSize() {return size;}
 
@@ -37,33 +34,33 @@ Grid::Grid(const string filename) { setGrid(filename); }
 void Grid::setGrid(const string filename)
 {
 	// open file
-	ifstream gridfile(filename);
+	ifstream gridfile(filename.c_str());
 	// attempt to read line with size of file
 	string line;
 	if(gridfile.is_open())
 	{
-		getline(myfile,line); // read first line
+		getline(gridfile,line); // read first line
 		char * pch =strtok(line," "); // split string by space character token
-		istringstream convert(pch); // stringstream used for the conversion constructed with the contents of 'pch' 
+		istringstream convert(pch); // stringstream used for the conversion constructed with the contents of 'pch'
                              		// ie: the stream will start containing the characters of 'pch'
 		if ( !(convert >> size) ) // give the value to 'size' using the characters in the stream
     	{ throw fileError("Unable to read grid size!"); }    // if that fails, throw error
 	}
 	else { throw fileOpenError("Unable to open grid file!"); }
-	
+
 	// size the data vectors
 	lttrs.resize(size,size); // size number of rows and columns
 
 	// read the characters from the rest of the file
 	char *ch; // temporary free character
-	for(int i=0;i<size,i++)
+	for(int i=0;i<size;i++)
 	{
 		for(int j=0;j<size;j++)
 		{
 			gridfile.read(ch,1); // read a character
 			if(*ch = '\s') // ignore spaces
 			{ j--; } // move index back for spaces
-			else if((*ch = '\n') || (*ch = '\r')) // skip to next row if encountering new line or return 
+			else if((*ch = '\n') || (*ch = '\r')) // skip to next row if encountering new line or return
 			{ break; } // move to next row
 			else
 			{ lttrs[i][j] = *ch; } // store character
@@ -75,3 +72,8 @@ void Grid::setGrid(const string filename)
 	// close file
 	gridfile.close();
 }
+
+Grid::~Grid() {
+	// TODO Auto-generated destructor stub
+}
+
