faf8a8cf223075b53484fcc31da380aa5ebe1e34
tiusty <awagud@hotmail.com>
2015-02-08 16:30:20 -0500

    Binary search abstracted to allow searching for varying criteria; includes string matching definitions

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,7 +2,7 @@
  * binarySearch.cpp
  *
  *  Created on: Feb 6, 2015
- *      Author: alex
+ *      Author: Alex, Thurston
  */
 
 #include "binarySearch.h"
@@ -10,44 +10,46 @@ using namespace std;
 #include <string>
 #include <vector>
 
-bool binSearch(vector <string> wordlist, string wordTarget, int low, int high)
+bool binSearch(vector <string> wordlist, string wordTarget, int low, int high, bool (*op) (string,string))
 {
 	int mid = ((low + high)/2);
 	bool result;
 
-	if(low > high)
-	{
-		result = false;
-	}
+	if(low > high)	{ return false;	}  // out of bounds, not here
+
+
+	if((*op) (wordTarget, wordlist[mid])) // found it!
+	{ return true; }
 	else
 	{
-		if(wordTarget==wordlist[mid])
+		if(wordTarget<wordlist[mid]) // search lower side
 		{
-			result = true;
+			result = binSearch(wordlist,wordTarget,low,mid-1,op);
 		}
-		else
+		else // search upper side
 		{
-			if(wordTarget<wordlist[mid])
-			{
-				result = binSearch(wordlist,wordTarget,low,mid-1);
-			}
-			else
-			{
-				if(wordTarget>wordlist[mid])
-				{
-					result =  binSearch(wordlist, wordTarget,mid+1,high);
-				}
-				else
-				{
-					result =  false;
-				}
-			}
-
+			result =  binSearch(wordlist, wordTarget,mid+1,high,op);
 		}
 	}
+
 	return result;
-
-
 }
 
+// returns whether two strings are equal
+bool stringEqual(string lhs, string rhs) {return lhs == rhs; }
+
+// checks if 'pre' is a prefix for the word
+bool stringPrefix(string pre, string word)
+{
+	// check that the sizes are consistent
+	bool match = (pre.size() <= word.size());
+	if(match)
+	{
+		// implement character by character checking across 'pre' and 'word'
+		for(int i=0;i<pre.size();i++)
+		{ match = match && (pre[i]==word[i]); }
+	}
+	// return result of checks
+	return match;
+}
 
