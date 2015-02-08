637fba36b7549733dc98e21fd344d08ceca63347
tiusty <awagud@hotmail.com>
2015-02-08 12:15:40 -0500

    creation of Grid class header file
    Created header file for Grid Class

---
+++ Grid.h
@@ -0,0 +1,26 @@
+/*
+ * Grid.h
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: alex
+ */
+
+#ifndef GRID_H_
+#define GRID_H_
+#include <string>
+#include "matrix.h"
+
+class Grid {
+public:
+	Grid();
+	Grid(const string filename);
+	void setGrid(const string filename);
+	const char getChar(const int m, const int n);
+	const int getSize();
+	virtual ~Grid();
+protected:
+		matrix<char> lttrs;
+		int size;
+};
+
+#endif /* GRID_H_ */
