4b14d08bef7479248c728a3a22c6d18e700dea81
tiusty <awagud@hotmail.com>
2015-02-09 03:29:54 -0500

    Create search.h

---
+++ search.h
@@ -0,0 +1,15 @@
+/*
+ * search.h
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: alex
+ */
+
+#ifndef SEARCH_H_
+#define SEARCH_H_
+
+
+void search(const int gridsize, const int sortType);
+
+
+#endif /* SEARCH_H_ */
