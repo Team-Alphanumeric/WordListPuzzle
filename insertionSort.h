ead118ea7524564217b8c9f69dafb34e7834302c
tiusty <awagud@hotmail.com>
2015-02-09 03:27:21 -0500

    Create insertionSort.h

---
+++ insertionSort.h
@@ -0,0 +1,15 @@
+/*
+ * insertionSort.h
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: alex
+ */
+
+#ifndef INSERTIONSORT_H_
+#define INSERTIONSORT_H_
+
+
+template <typename T> void insertionSort(std::vector<T> &arr);
+
+
+#endif /* INSERTIONSORT_H_ */
