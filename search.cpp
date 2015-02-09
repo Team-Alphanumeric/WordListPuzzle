47e811a6756af66abf3197f1385ea3a02679832d
tiusty <awagud@hotmail.com>
2015-02-09 13:57:17 -0500

    Added comments

--- search.cpp
+++ search.cpp
@@ -19,7 +19,9 @@
 
 void search(const int sortType, const int gridsize=15)
 {
+	// declare a string which holds the file name
 	std::string gridname;
+	//switch statement to decide the size of the grid based off the integer inputed by the user
 	switch(gridsize)
 	{
 		case 15: gridname = "input15.txt"; break;
@@ -28,11 +30,17 @@ void search(const int sortType, const int gridsize=15)
 		case 250: gridname = "input250.txt"; break;
 		default: gridname = "input15.txt"; break;
 	}
+	//declare classes
 	Grid g(gridname);
 	clock_t start = clock(), sorttime, searchtime;
 	Wordlist w("wordList.txt");
+	//sorts the list based off the inputed value of the user. 0= InsertionSort, 1=QuickSort, 2=mergeSort
+	//calculate the CPUS time to sort
 	w.sortList(sortType); sorttime = clock();
+	//find matches in the sorted wordlist
+	//calculate the CPU time to search
 	findMatches(w,g); searchtime = clock();
+	//print out eh CPU times
 	cout << "Sorting Time: " << (float) (sorttime-start)/CLOCKS_PER_SEC << endl;
 	cout << "Search Time: " << (float) (searchtime-sorttime)/CLOCKS_PER_SEC << endl;
 	cout << "Total Time: " <<(float) (clock()-start)/CLOCKS_PER_SEC << endl;
