9c25efef74b06114213f7cb7ec79a0dd3ff620ec
tiusty <awagud@hotmail.com>
2015-02-09 03:28:11 -0500

    Create mergeSort.h

---
+++ mergeSort.h
@@ -0,0 +1,15 @@
+/*
+ * mergeSort.h
+ *
+ *  Created on: Feb 9, 2015
+ *      Author: alex
+ */
+
+#ifndef MERGESORT_H_
+#define MERGESORT_H_
+
+
+template <typename T> vector<T> mergeSort(vector <T>& list, int left, int right);
+
+
+#endif /* MERGESORT_H_ */
