e0e63afcdf1d3092148efc26379eed863f79023c
thorstone25 <tbrevett@aol.com>
2015-02-09 14:02:45 -0500

    Added exceptions and algorithm comments

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -12,27 +12,31 @@ using namespace std;
 
 bool binSearch(vector <string> wordlist, string wordTarget, int low, int high, bool (*op)(string,string))
 {
+	// determine partition bounds
 	int mid = ((low + high)/2);
-	bool result;
+	
+	if(low > high)	{ return false;	}  // base case: index out of bounds, not here
 
-	if(low > high)	{ return false;	}  // out of bounds, not here
-
-
-	if((*op) (wordTarget, wordlist[mid])) // found it!
-	{ return true; }
+	// double check partition bounds
+	if((low<0) || (low>=wordlist.size()) {throw indexRangeError("In 'binSearch': Lower index out of bounds"); }
+	if((mid<0) || (mid>=wordlist.size()) {throw indexRangeError("In 'binSearch': Middle index out of bounds"); }
+	if((high<0) || (high>=wordlist.size()) {throw indexRangeError("In 'binSearch': Upper index out of bounds"); }
+	
+	
+	if((*op) (wordTarget, wordlist[mid])) { return true; } // success of operand: found it!
 	else
 	{
 		if(wordTarget<wordlist[mid]) // search lower side
 		{
-			result = binSearch(wordlist,wordTarget,low,mid-1,op);
+			return binSearch(wordlist,wordTarget,low,mid-1,op);
 		}
 		else // search upper side
 		{
-			result =  binSearch(wordlist, wordTarget,mid+1,high,op);
+			return binSearch(wordlist, wordTarget,mid+1,high,op);
 		}
 	}
 
-	return result;
+	return false;
 }
 
 // returns whether two strings are equal
@@ -41,11 +45,12 @@ bool stringEqual(string lhs, string rhs) {return lhs == rhs; }
 // checks if 'pre' is a prefix for the word
 bool stringPrefix(string pre, string word)
 {
-	// check that the sizes are consistent
-	bool match = (pre.size() < word.size());
+	// check that the sizes are consistent with 'pre' being a prefix
+	bool match = pre.size() < word.size();
 	if(match)
 	{
 		// implement character by character checking across 'pre' and 'word'
+		// only traverses the length of 'pre' since 'word' is longer than 'pre'
 		for(int i=0;i<pre.size();i++)
 		{ match = match && (pre[i]==word[i]); }
 	}
