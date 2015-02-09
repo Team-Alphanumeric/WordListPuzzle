954286911f6cd007959be2ae783f391ca48c0d04
tiusty <awagud@hotmail.com>
2015-02-09 03:31:12 -0500

    Create WordListPuzzle.cpp

---
+++ WordListPuzzle.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : WordListPuzzle.cpp
+// Author      : Alex Agudelo, Thurston Brevett
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include "Wordlist.h"
+#include "Grid.h"
+#include "findMatches.h"
+#include "insertionSort.h"
+#include "search.h"
+
+int main(void)
+{
+	search(1, 15);
+	return 0;
+}
