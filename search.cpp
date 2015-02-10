4248f7234317d836c967f5188d8a7ea05f95589e
tiusty <awagud@hotmail.com>
2015-02-10 13:06:05 -0500

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -1,52 +1,45 @@
-/*
- * search.cpp
- *
- *  Created on: Feb 8, 2015
- *      Author: Alex, Thurston
- */
+//============================================================================
+// Name        : WordListPuzzle.cpp
+// Author      : Alex Agudelo, Thurston Brevett
+// Version     : Project 2a
+// Copyright   :
+// Description : Executes a word search on a grid of letters corresponding to
+// 				 a list of words
+//============================================================================
 
-#include <time.h>
-#include <cstring>
-#include <string>
 #include <iostream>
 #include <stdexcept>
-#include <cstdio>
-#include <stdlib.h>
-#include "Grid.h"
+#include <exception>
+#include "d_except.h"
 #include "Wordlist.h"
+#include "Grid.h"
 #include "findMatches.h"
+#include "insertionSort.h"
+#include "search.h"
 using namespace std;
 
-void search(const int sortType, const int gridsize=15)
+// executes a word search
+int main(void)
 {
-	// declare a string which holds the file name
-	std::string gridname;
-
-	//switch statement to decide the size of the grid based off the integer inputed by the user
-	if(gridsize<0) {throw invalid_argument("in 'search': invalid argument"); }
-	switch(gridsize)
+	try
 	{
-		case 15: gridname = "input15.txt"; break;
-		case 30: gridname = "input30.txt"; break;
-		case 50: gridname = "input50.txt"; break;
-		case 250: gridname = "input250.txt"; break;
-		default: gridname = "input15.txt"; break;
+		// execute a search
+		// arg1: 0 -> insertion sort, 1 -> quick sort, 2-> merge sort
+		// arg2: the puzzle number, 1-8, with puzzle size, 15,30,50,50,100,250,500,1000
+		search(2, 8);
+		return 0;
 	}
-	//declare classes
-	Grid g(gridname);
-	Wordlist w("wordList.txt");
-
-	clock_t start = clock(), sorttime, searchtime;
-
-	// sort the list based off the inputed value: 0= InsertionSort, 1=QuickSort, 2=mergeSort
-	w.sortList(sortType); sorttime = clock(); // get the time after sorting
-
-	// find matches in the sorted wordlist
-	findMatches(w,g); searchtime = clock(); // get the time after searching
-
-	// print out time spent for each operation
-	cout << "Sorting Time: " << (float) (sorttime-start)/CLOCKS_PER_SEC << "s" << endl;
-	cout << "Search Time: " <<  (float) (searchtime-sorttime)/CLOCKS_PER_SEC << "s" << endl;
-	cout << "Total Time: " <<   (float) (clock()-start)/CLOCKS_PER_SEC << "s" << endl;
+	catch(exception &e)
+	{
+		cout <<"In 'main': " << e.what() << endl;
+	}
+	catch(baseException &e)
+	{
+		cout <<"In 'main': " << e.what() << endl;
+	}
+	catch(...)
+	{
+		cout<<"In 'main': Unknown exception " << endl;
+	}
+	return -1;
 }
-
