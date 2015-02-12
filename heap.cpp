69cddcece0358b536901e77ad26ed4fa0bbfd891
thorstone25 <tbrevett@aol.com>
2015-02-12 16:05:33 -0500

    Included definitions for Heap functions

--- heap.cpp
+++ heap.cpp
@@ -1,1 +1,14 @@
 // filler text
+
+			
+heap::heap();
+
+heap::heap(vector<T> input);
+
+
+void heap::buildMaxHeap(); // turns a heap structure into a valid heap
+
+void heap::maxHeapify(int i); // makes a heap valid
+
+
+
