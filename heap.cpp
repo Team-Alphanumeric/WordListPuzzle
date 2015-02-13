beb63660ca51a2a853074ad9ae563166e427f605
thorstone25 <tbrevett@aol.com>
2015-02-13 01:51:29 -0500

    All heap functions defined for proj. 2b

--- heap.cpp
+++ heap.cpp
@@ -1,14 +1,101 @@
 // filler text
 
+// do nothing default constructor
+heap::heap(){}
+
+// inputs and initializes data to a maximum root heap
+heap::heap(vector<T> input)
+{ initializeMaxHeap(input); buildMaxHeap(); return; }
+
+// inputs and initializes data to a heap of criteria 'op'
+heap::heap(vector<T> input, bool (*op)(T,T))
+{ initializeMaxHeap(input); buildHeap(op); return; }
+
+
+// builds a maximum root heap from an unstructered array
+/* in the future, this should just call 'buildHeap' */
+void heap::buildMaxHeap() // turns a heap structure into a valid heap
+{
+	/*
+	- initialize heap structure size to full array of data
+	- move from last index with children to first index creating valid heaps
+	*/
+	structuredheapsize = data.size(); 
+	for(int i = parent(structuredheapsize-1); i<0; i--) { maxHeapify(i); } return;
+}
+
+// builds a heap from an unstructered array according to criteria 'op'
+void heap::buildHeap( bool (*op)(T,T) )
+{
+	/*
+	- initialize heap structure size to full array of data
+	- move from last index with children to first index creating valid heaps
+	*/
+	structuredheapsize = data.size(); 
+	for(int i = parent(structuredheapsize-1); i<0; i--) { heapify(i,op); } return;
+}
+// creates a maximum root heap of root i
+/* in the future, this should just call 'heapify' */
+void heap::maxHeapify(int i) // makes a heap with root i valid
+{
+	int l = left(i), r=right(i); // left, right index: valid if >= 0 
+	T big=data[i]; int ibig=i; // largest value and coresponding index: defaults to root
+	
+	if((r>=0) && (data[r]>big)) {big=data[r]; ibig=r;} // find largest node/node-index
+	if((l>=0) && (data[l]>big)) {big=data[l]; ibig=l;}
+	
+	if(ibig != i) // if the root isn't the largest
+	{	dataSwap(i,ibig); maxHeapify(ibig); } // swap values, validate the swapped subheap
+	return;
+}
+
+// makes a heap with root 'i' valid according to criteria 'op'
+void heap::heapify(int i, bool (*op)(T,T))
+{
+	int l = left(i), r = right(i); // left, right index: valid if >= 0 
+	T big=data[i]; int ibig=i; // largest value and coresponding index: defaults to root
+	
+	if( (r>=0) && (*op)(data[r],big) ) { big=data[r]; ibig=r;} // find largest node/node-index
+	if( (l>=0) && (*op)(data[l],big) ) { big=data[l]; ibig=l;}
+	
+	// if the root isn't the largest swap values and validate the subheap of the swapped value
+	if(ibig != i) {	dataSwap(i,ibig); heapify(ibig,op); } 	return;
+}
+
+
+/* in the future, this should call 'heapSort' */
+void heap::maxHeapSort()
+{
+	/* 
+	- initialize heap structure size to full array of data & ensure we start with a fully valid heap
+	- then, move through the array from back to front creating a list from the back of the heap
+	- for each iteration: move top item to the back of the list and decrement the size of the array that is
+	  considered part of the heap structure: this affects the 'left', 'right' functions
+	- then, recreate a valid heap	
+	*/
+	structuredheapsize = data.size(); buildHeap(op);
+	for(int i = structuredheapsize-1; i<0; i--)
+	{ dataSwap(0,(structuredheapsize--,structuredheapsize)); 	maxHeapify(0); } return;
+}
+
+// sort the data according to the criteria
+void heap::heapSort( bool (*op)(T,T) )
+{
+	/* 
+	- initialize heap structure size to full array of data & ensure we start with a fully valid heap
+	- then, move through the array from back to front creating a list from the back of the heap
+	- for each iteration: move top item to the back of the list and decrement the size of the array that is
+	  considered part of the heap structure: this affects the 'left', 'right' functions
+	- then, recreate a valid heap	
+	*/
+	structuredheapsize = data.size(); buildHeap(op);
+	for(int i = structuredheapsize-1; i<0; i--)
+	{ dataSwap(0,(structuredheapsize--,structuredheapsize)); 	heapify(0,op); } return;
+}
+
+// swap values of data
+void heap::dataSwap(int i, int j)
+{ T temp = data[i];	data[i] = data[j];	data[j] = temp;	return; } /* swap values */ 			
 			
-heap::heap();
-
-heap::heap(vector<T> input);
-
-
-void heap::buildMaxHeap(); // turns a heap structure into a valid heap
-
-void heap::maxHeapify(int i); // makes a heap valid
-
-
-
+			
+			
--- heap.h
+++ heap.h
@@ -1,25 +1,33 @@
-// filler text
-template <typename T>
+
+
+template <typename T> 
 class heap // make heap a vector with expanded data & functionality
 {
 		public:
 			heap();
 			heap(vector<T> input);
+			heap(vector<T> input, bool (*op)(T,T));
 			int parent(int i) { return ((i-1)/2); } // gets the index of the parent
 			int left(int i) {return (int a=(2*(i+1)-1) , (a<sortedheapsize) ? a : -1 ); } // gets the index of the left heap: -1 for non existent heaps
 			int right(int i) {return (int a=(2*(i+1)) , (a<sortedheapsize) ? a : -1 ); } // get the index of the right heap: -1 for non existent heaps
 			T getItem(int i) {return data[i]; } // gets the T(hing) at the index
 			void setItem(int i, T gj) {data[i] = gj;} // sets the T(hing) at the index
-			initializeMaxHeap(vector<T> input) {data=input;} // accepts a vector of T(hings)
-			int size() {return data.size();}
-			void buildMaxHeap(); // turns a heap structure into a valid heap
-			void maxHeapify(int i); // makes a heap valid
-			int getHeapSize() {return sortedheapsize;} // get the heap size
-			int setHeapSize(int n) {sortedheapsize=n;} // set the heap size
-//			void resize(int n) { data.resize(n); } // resize the vector
-//			void push_back(T gj) {data.push_back(gj);} // pushes an item to the back of the list
+			void initializeMaxHeap(vector<T> input) {data=input;} // accepts a vector of T(hings)
+			int getHeapSize() {return structuredheapsize;} // get the heap size
+			int setHeapSize(int n) {structuredheapsize=n;} // set the heap size
+			int size() {return data.size();} // get the vector size
+			void buildMaxHeap <T>(); // turns a heap structure into a valid heap
+			void buildHeap ( bool (*op)(T,T) ); // builds a heap according to the given criteria
+			void maxHeapify (int i); // makes a heap valid
+			void heapify( bool (*op)(T,T) ); // makes a heap valid according to the given criteria
+			void maxHeapSort();
+			void heapSort( bool (*op)(T,T) );
+			void dataSwap(int i, int j);
+//			void push_back(T gj) {data.push_back(gj);} // pushes an item to the bottom of the heap (back of the vector) 
 				
 		protected: 
 			vector<T> data;
-			int sortedheapsize;
+			int structuredheapsize;
 }
+
+ 
