1884d531cb36288d8ea00a4be5f286684f9defbe
tiusty <awagud@hotmail.com>
2015-02-19 12:22:17 -0500

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -1,45 +1,57 @@
-//============================================================================
-// Name        : WordListPuzzle.cpp
-// Author      : Alex Agudelo, Thurston Brevett
-// Version     : Project 2a
-// Copyright   :
-// Description : Executes a word search on a grid of letters corresponding to
-// 				 a list of words
-//============================================================================
+/*
+ * search.cpp
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: Alex, Thurston
+ */
 
+#include <time.h>
+#include <cstring>
+#include <string>
 #include <iostream>
 #include <stdexcept>
-#include <exception>
-#include "d_except.h"
-#include "Wordlist.h"
+#include <cstdio>
+#include <stdlib.h>
 #include "Grid.h"
+#include "Wordlist.h"
 #include "findMatches.h"
-#include "insertionSort.h"
-#include "search.h"
 using namespace std;
 
-// executes a word search
-int main(void)
+void search(const int sortType, const int puzzleNum=1)
 {
-	try
+	// declare a string which holds the file name
+	std::string gridname;
+
+	//switch statement to decide the size of the grid based off the integer inputed by the user
+	if(puzzleNum<0 || puzzleNum>8) {throw invalid_argument("in 'search': invalid argument"); }
+	switch(puzzleNum)
 	{
-		// execute a search
-		// arg1: 0 -> insertion sort, 1 -> quick sort, 2-> merge sort
-		// arg2: the puzzle number, 1-8, with puzzle size, 15,30,50,50,100,250,500,1000
-		search(2, 8);
-		return 0;
+		case 1: gridname = "input15.txt"; break;
+		case 2: gridname = "input30.txt"; break;
+		case 3: gridname = "input50.txt"; break;
+		case 4: gridname = "puzzle3_size50.txt"; break;
+		case 5: gridname ="puzzle4_size100.txt"; break;
+		case 6: gridname = "input250.txt"; break;
+		case 7: gridname = "puzzle5_size500.txt"; break;
+		case 8: gridname = "puzzle6_size1000.txt"; break;
+		default: gridname = "input15.txt"; break;
 	}
-	catch(exception &e)
-	{
-		cout <<"In 'main': " << e.what() << endl;
-	}
-	catch(baseException &e)
-	{
-		cout <<"In 'main': " << e.what() << endl;
-	}
-	catch(...)
-	{
-		cout<<"In 'main': Unknown exception " << endl;
-	}
-	return -1;
+	cout << "Searching Puzzle " << gridname << endl;
+	//declare classes
+	Grid g(gridname);
+	Wordlist w("wordList.txt");
+
+	clock_t start = clock(), sorttime, searchtime;
+
+	// sort the list based off the inputed value: 0= InsertionSort, 1=QuickSort, 2=mergeSort
+	w.sortList(sortType); sorttime = clock(); // get the time after sorting
+
+	// find matches in the sorted wordlist
+	findMatches(w,g); searchtime = clock(); // get the time after searching
+
+	// print out time spent for each operation
+	cout << "Sorting Time: " << (float) (sorttime-start)/CLOCKS_PER_SEC << "s" << endl;
+	cout << "Search Time: " <<  (float) (searchtime-sorttime)/CLOCKS_PER_SEC << "s" << endl;
+	cout << "Total Time: " <<   (float) (clock()-start)/CLOCKS_PER_SEC << "s" << endl;
 }
+
