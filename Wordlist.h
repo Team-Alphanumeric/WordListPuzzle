10765f30b42c2e67d7ec73397c8d4240db679e3d
tiusty <awagud@hotmail.com>
2015-02-22 13:52:56 -0500

    Update Wordlist.h

--- Wordlist.h
+++ Wordlist.h
@@ -12,14 +12,23 @@ using namespace std;
 #include <vector>
 #include "insertionSort.h"
 #include "quickSort.h"
+#include "HashTable.h"
 
 class Wordlist {
+private:
+	unsigned int longWordLength;
 protected:
 	vector <string> wds;
+	HashTable wdsHs;
+	int dataType;
 public:
 	Wordlist();
 	Wordlist(string filename);
 	void readWords(string filename);
+	void setDataType(int newDataType);
+	int getDataType();
+	bool validLength(string word);
+	void readWordsHash(string filename);
 	void printWords();
 	int getVectorSize();
 	bool binSearchWordList(vector <string> wordlist, string wordTarget);
@@ -28,6 +37,7 @@ public:
 	bool prefix(string word1);
 	void sortList(int opt=0);
 	string getWord(const int index);
+	unsigned int getLongWordLength();
 	friend ostream& operator<< (ostream &ostr, Wordlist w1);
 	virtual ~Wordlist();
 };
